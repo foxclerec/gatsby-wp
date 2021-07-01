34781d9c7066e1de5036ac0caac59aae5149f2df
Anatolii Skoryk <anatolii.skoryk@gmail.com>
2021-07-01 16:06:11 +0200

    About, CTA, Quote, Latest Post sections added

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,10 @@ import * as React from "react"
 import SEO from "../components/SEO/SEO"
 import Layout from '../components/Layout/Layout'
 import Hero from '../components/Hero/Hero'
+import CTAArea from '../components/CTAArea/CTAArea'
+import LatestBlogPost from '../components/LatestBlogPost/LatestBlogPost'
+import Quote from '../components/Quote/Quote'
+import About from '../components/About/About'
 
 
 
@@ -9,6 +13,10 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <Hero />
+    <CTAArea />
+    <LatestBlogPost />
+    <Quote />
+    <About />
   </Layout>
 )
 
